757c9e80301f939df9343052e509042b287cd6a3
Behnam Nikbakht <b.nikbakht@artin.ai>
2021-12-21 21:47:02 -0700

    not token

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -1,6 +1,7 @@
 import argparse
 import pickle
 import nltk
+from nltk import ngrams
 from nltk import PorterStemmer, LancasterStemmer, pos_tag
 from nltk.corpus import wordnet as wn
 from nltk.stem import WordNetLemmatizer
@@ -10,23 +11,51 @@ from data_loader import DataLoader
 
 stopwords = nltk.corpus.stopwords.words("english")
 
+
+class Config:
+    def __init__(self, log, ngrams_factor, tokenize_not):
+        self.log = log
+        self.ngrams_factor = ngrams_factor
+        self.tokenize_not = tokenize_not
+
+
 # this class represents a unit for analyzing tweet (or any text in general)
 class TextItem:
     # constructor
     def __init__(self, text):
         self.text = text
 
-    def tokenize(self, log=True):
-        if log:
+    def tokenize(self, config):
+        if config.log:
             print("start tokenize {}".format(self.text))
         # tokenize by space and new-line, and normalize by converting to lowercase
         tokens = [w.lower() for w in nltk.word_tokenize(self.text)]
-        if log:
+        if config.log:
             print("tokens after wordnet tokenize = {}".format(tokens))
+
+        # n-grams
+        tokens = [' '.join(grams) for grams in ngrams(tokens, config.ngrams_factor)]
+        if config.log:
+            print("after n-grams {}".format(tokens))
+
+        # tokenize not
+        if config.tokenize_not:
+            temp = []
+            i = 0
+            while i < len(tokens) - 1:
+                if tokens[i] == 'not':
+                    temp.append(tokens[i] + ' ' + tokens[i+1])
+                    i = i + 2
+                else:
+                    temp.append(tokens[i])
+                    i = i + 1
+            tokens = temp
+            if config.log:
+                print("after tokenize not {}".format(tokens))
 
         # remove stop words from tokens, and single-character tokens
         tokens = [w for w in tokens if w not in stopwords and len(w) > 1]
-        if log:
+        if config.log:
             print("tokens after stop words removal = {}".format(tokens))
 
         return tokens
@@ -38,7 +67,7 @@ class TextItem:
         # extract lingusitic position of each token
         tagged = pos_tag(tokens)
 
-        #result = [lemmatizer.lemmatize(word) for word,tag in tagged if tag in ["NN", "NNS"]]
+        # result = [lemmatizer.lemmatize(word) for word,tag in tagged if tag in ["NN", "NNS"]]
         # map position to the required input for lemmatizer, based on the first character of the tag_pos
         tag_map = defaultdict(lambda: wn.NOUN)
         tag_map['J'] = wn.ADJ
@@ -65,20 +94,13 @@ class TextItem:
 
         return result, synonyms
 
-    def preprocessing(self, log=True):
-        tokens = self.tokenize(log)
+    def preprocessing(self, config):
+        tokens = self.tokenize(config)
         self.tokens, self.synonyms = self.normalize(tokens)
 
         # frequency of tokens
         self.frequency = nltk.FreqDist([w.lower() for w in self.tokens])
 
-
-# text = """
-# i am feeling good today because the weather is perfect"""
-# t = TextItem(text)
-# t.preprocessing()
-#
-# print("tokens = {}, synonyms = {}, frequency = {}".format(t.tokens, t.synonyms, t.frequency.items()))
 
 class Classifier(object):
 
@@ -86,7 +108,7 @@ class Classifier(object):
         self.data_loader = DataLoader()
 
     def getLabeledDataset(self):
-        dataset = self.data_loader.load2()
+        dataset = self.data_loader.load1()
         size = len(dataset)
         test_set_size = int(size / 10)
         print("size of all labeled labeled_dataset = {}".format(size))
@@ -98,24 +120,24 @@ class NltkClassifier(Classifier):
     def __init__(self):
         super(NltkClassifier, self).__init__()
 
-    def preparation(self):
+    def preparation(self, config):
         plain_train_set, plain_test_set = self.getLabeledDataset()
         print("train_set = {}, test_set = {}".format(len(plain_train_set), len(plain_test_set)))
 
         self.train_set = []
         self.test_set = []
 
-        for p,c in plain_train_set:
+        for p, c in plain_train_set:
             t = TextItem(p)
-            t.preprocessing(False)
-            self.train_set.append((t,c))
+            t.preprocessing(config)
+            self.train_set.append((t, c))
 
-        for p,c in plain_test_set:
+        for p, c in plain_test_set:
             t = TextItem(p)
-            t.preprocessing(False)
-            self.test_set.append((t,c))
+            t.preprocessing(config)
+            self.test_set.append((t, c))
 
-        self.classifier = nltk.NaiveBayesClassifier.train([(listToDict(t.tokens),c) for t,c in self.train_set])
+        self.classifier = nltk.NaiveBayesClassifier.train([(listToDict(t.tokens), c) for t, c in self.train_set])
 
         with open('trained_model', 'wb') as trained_model_file:
             pickle.dump(self.classifier, trained_model_file)
@@ -127,39 +149,43 @@ class NltkClassifier(Classifier):
             if cls == c:
                 stat["correct"] = stat["correct"] + 1
             stat["all"] = stat["all"] + 1
-            #print("test = {}, c = {}".format(cls, c))
+            # print("test = {}, c = {}".format(cls, c))
 
         print("stat = {}, accuracy = {}%".format(stat, 100 * stat["correct"] / stat["all"]))
 
-    def predict(self, text):
+    def predict(self, text, config):
         with open('trained_model', 'rb') as trained_model_file:
             trained_model = pickle.load(trained_model_file)
             p = TextItem(text)
-            p.preprocessing()
+            config.log = True
+            p.preprocessing(config)
             return trained_model.classify(listToDict(p.tokens))
+
 
 def listToDict(l):
     return {l[i]: l[i] for i in range(len(l))}
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Tweet Emotion Recognition')
     parser.add_argument('--command', choices=['test', 'build', 'predict'], help='t [run test], p [predict]')
     parser.add_argument('--text', help='input text for predict')
+    parser.add_argument('--ng', type=int, default=1, help='n-grams factor')
+    parser.add_argument('--tokno', type=bool, default=False, help='tokenize not verbs in a single token')
     args = parser.parse_args()
 
     nltkClassifier = NltkClassifier()
 
     print("args = {}".format(args))
 
+    config = Config(log=False, ngrams_factor=args.ng, tokenize_not=args.tokno)
+
     if args.command == 'build':
-        nltkClassifier.preparation()
+        nltkClassifier.preparation(config)
     elif args.command == 'test':
-        nltkClassifier.preparation()
+        nltkClassifier.preparation(config)
         nltkClassifier.test()
     elif args.command == 'predict':
         text = args.text
-        result = nltkClassifier.predict(text)
+        result = nltkClassifier.predict(text, config)
         print("result = {}".format(result))
-
-
-
